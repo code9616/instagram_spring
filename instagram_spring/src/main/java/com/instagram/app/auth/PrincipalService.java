ac1dc2170ed63a80f335fad7190db46a4ea75c2d
ITPS <ITPS@gmail.com>
2022-04-28 13:07:57 +0900

    비밀번호 변경

--- instagram_spring/src/main/java/com/instagram/app/auth/PrincipalService.java
+++
@@ -1,8 +0,0 @@
-package com.instagram.app.auth;
-
-import com.instagram.app.domain.user.User;
-
-public interface PrincipalService {
-	public User loadUserByUsername(String username);
-	public boolean passwordCheck(String password, User user);
-}
